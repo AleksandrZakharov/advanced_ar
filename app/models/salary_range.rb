573601f949e1d7d668338da1ccc496755dbbd5de
human.of.rain <hauman.of.rain@gmail.com>
2016-02-26 16:48:17 +0300

    Validations

--- app/models/salary_range.rb
+++ app/models/salary_range.rb
@@ -1,3 +1,11 @@
 class SalaryRange < ActiveRecord::Base
   belongs_to :job
+
+  validate :min_is_less_then_max
+
+  def min_is_less_then_max
+  	if min_salary > max_salary
+  		errors.add(:min_salary, "cannot be greater than maximum salary!")
+  	end
+  end
 end
